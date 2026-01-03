4a281d1acdacec7ddf37fc910e42d092b6ced99f
Sharanya Maddha <sharanyamaddha@gmail.com>
2026-01-03 19:45:17 +0530

    updated files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/public/home/home';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+import { AdminDashboard } from './features/admin/admin-dashboard/admin-dashboard';
+import { NoAuthGuard } from './core/guards/no-auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'home', component: Home },
+  { path: 'login', component: Login, canActivate: [NoAuthGuard] },
+  { path: 'register', component: Register, canActivate: [NoAuthGuard] },
+  {
+    path: 'admin',
+    children: [
+      { path: 'dashboard', component: AdminDashboard }
+    ]
+  }
+];
